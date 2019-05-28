119db3b1b9539ad46704f7901e368dbf453f6489
Gideon Nnalue <emekannalue@gmail.com>
2019-05-27 22:18:53 -0400

    Updates

---
+++ precache-manifest.58d9f026175a35f0c42a2442f43c0aff.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebf06ec119d95862146ca850aac47bb4",
+    "url": "/netflix/index.html"
+  },
+  {
+    "revision": "6de1468f168f76580885",
+    "url": "/netflix/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b2d0fe2dff9ccd0c8fe9",
+    "url": "/netflix/static/css/main.cdc335cd.chunk.css"
+  },
+  {
+    "revision": "6de1468f168f76580885",
+    "url": "/netflix/static/js/2.b8ab2249.chunk.js"
+  },
+  {
+    "revision": "b2d0fe2dff9ccd0c8fe9",
+    "url": "/netflix/static/js/main.b26a5190.chunk.js"
+  },
+  {
+    "revision": "6a60a5860bbbfa2dbce1",
+    "url": "/netflix/static/js/runtime~main.f479a0f0.js"
+  },
+  {
+    "revision": "f0fbf3636060bed0e73fb6bd73eae65c",
+    "url": "/netflix/static/media/escobar.f0fbf363.jpg"
+  },
+  {
+    "revision": "dfcb3dd08880844ce01463b18a5be54a",
+    "url": "/netflix/static/media/logo.dfcb3dd0.png"
+  },
+  {
+    "revision": "5d0f7fbbe6c795b19f8f21a54ea470e6",
+    "url": "/netflix/static/media/narcos_logo.5d0f7fbb.png"
+  },
+  {
+    "revision": "59298547ea0f08dfd41b24166e97f0f0",
+    "url": "/netflix/static/media/poster.59298547.jpg"
+  },
+  {
+    "revision": "372236176a2840220e70bb497b23b170",
+    "url": "/netflix/static/media/test.37223617.jpg"
+  },
+  {
+    "revision": "7a21cbe90fd31540fb0384792fbc4a86",
+    "url": "/netflix/static/media/user.7a21cbe9.svg"
+  }
+]);
